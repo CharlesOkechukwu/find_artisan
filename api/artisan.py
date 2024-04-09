024d79bdc8343d9675b2a9e7886431caaf0825f7
CharlesOkechukwu <realcharlieok@gmail.com>
2024-04-10 00:08:39 +0100

    add edit artisan function

--- api/artisan.py
+++ api/artisan.py
@@ -4,7 +4,7 @@ import os
 from flask import render_template, request, redirect, url_for, session, current_app
 from models import storage
 from . import fa_app
-from flask import jsonify
+from flask import jsonify, flash
 from werkzeug.utils import secure_filename
 from models.artisan import Artisan
 
@@ -116,7 +116,6 @@ def find_artisan():
 
         artisans = None
         if filter == "nearby":
-            print("nearby")
             objs = storage.get_by_city(city, service)
             artisans = []
             for obj in objs:
@@ -124,13 +123,60 @@ def find_artisan():
                 if distance < 30:
                     artisans.append(obj)
         elif filter == "in-city":
-            print("city")
             artisans = storage.get_by_city(city, service)
         elif filter == "in-state":
-            print("state")
             artisans = storage.get_by_state(state, service)
         else:
-            print("country")
             artisans = storage.get_by_country(country, service)
         return render_template('display.html', artisans=artisans)
+
+@fa_app.route("/artisan/delete/<a_id>", strict_slashes=False)
+def delete_artisan(a_id):
+    """delete an artisan from database"""
+    artisan = storage.get_artisan(a_id)
+    storage.delete(artisan)
+    storage.save()
+    flash("Service deleted sucessfully!")
+    return redirect(url_for('fa_app.account'))
     
+@fa_app.route("/artisan/edit/<a_id>", strict_slashes=False, methods=['POST', 'GET'])
+def edit_artisan(a_id):
+    """edit an artisan details"""
+    if session['loggedin'] == True:
+        artisan = storage.get_artisan(a_id)
+        if request.method == 'GET':
+            return render_template('edit_artisan.html', artisan=artisan)
+        else:
+            business_name = request.form['business_name']
+            service = request.form.get('service', False)
+            address = request.form['address']
+            city = request.form['city']
+            state = request.form['state']
+            country = request.form['country']
+            phone_number = request.form['phone_number']
+            email = request.form['email']
+            bio = request.form['bio']
+            if business_name != "":
+                artisan.business_name = business_name
+            if service != "":
+                artisan.service = service
+            if address != "":
+                artisan.address = address
+            if city != "":
+                artisan.city = city
+            if state != "":
+                artisan.state = state
+            if country != "":
+                artisan.country = country
+            if phone_number != "":
+                artisan.phone_number = phone_number
+            if email != "":
+                artisan.email = email
+            if bio != "":
+                artisan.bio = bio
+            storage.save()
+            flash("Service updated sucessfully!")
+            return redirect(url_for('fa_app.account'))
+
+    else:
+        return redirect(url_for('fa_app.login'))
--- api/home.py
+++ api/home.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """module to handle home page views for fa_app"""
-from flask import app, render_template, request
+from flask import app, render_template, request, session
 from models import storage
 from . import fa_app
 
@@ -8,4 +8,7 @@ from . import fa_app
 @fa_app.route("/", strict_slashes=False, methods=['GET', 'POST'])
 def home():
     """render home page"""
-    return render_template('index.html')
+    user = None
+    if session.get('loggedin')  == True:
+        user = storage.get_user(session['id'])
+    return render_template('index.html', user=user)
